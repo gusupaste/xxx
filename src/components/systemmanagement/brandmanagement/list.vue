09ebe10f2ff54c3e6f2188153770e7cac2178e74
mn <1076660242@qq.com>
2019-04-17 11:39:03 +0800

    commit

--- src/components/systemmanagement/brandmanagement/list.vue
+++ src/components/systemmanagement/brandmanagement/list.vue
@@ -1,23 +1,29 @@
 <template>
     <div class='brandmanagement'>
       <homepage></homepage>
-      <p>YOU ARE HERE : 系统管理 >> 品牌管理</p>
-      <div class="content-top">
-        <div class="top">
-          <span class="top-span">班级项目：</span>
-          <el-button class="el-button" type="primary">管理</el-button><br>
-          <span class="top-span-bot">双语班 ； 国际班 ； 慧智班 ； 慧乐班 ；慧智双语班级 ； 慧智普通班  </span>
-        </div>
-        <div class="top">
-          <span class="top-span">年级项目：</span>
-          <el-button class="el-button" type="primary">管理</el-button><br>
-          <span class="top-span-bot">国际班；双语班；</span>
+      <div class="brandmanagement-content">
+        <p>YOU ARE HERE : 系统管理 >> 品牌管理</p>
+        <div class="content-top">
+          <div class="top">
+            <span class="top-span">班级项目：</span>
+            <el-button class="el-button" type="primary">管理</el-button><br>
+            <span class="top-span-bot">双语班 ； 国际班 ； 慧智班 ； 慧乐班 ；慧智双语班级 ； 慧智普通班  </span>
+          </div>
+          <div class="top">
+            <span class="top-span">年级项目：</span>
+            <el-button class="el-button" type="primary">管理</el-button><br>
+            <span class="top-span-bot">国际班；双语班；</span>
+          </div>
         </div>
       </div>
     </div>
 </template>
 <style scoped>
-  .brandmanagement p{
+  .brandmanagement-content{
+    padding: 10px 25px;
+    margin: 0;
+  }
+  .brandmanagement-content p{
       left: 95px;
       top: 99px;
       height: 20px;
@@ -26,22 +32,22 @@
       text-align: left;
       font-family: SourceHanSansSC-regular;
   }
-  .brandmanagement .content-top{
+  .brandmanagement-content .content-top{
     min-height: 120px;
     border: 1px solid #ddd;
-    margin: 25px;
+    margin: 10px 0px;
     padding: 10px;
     text-align: left;
   }
-  .brandmanagement .top-span{
+  .brandmanagement-content .top-span{
     color: #006287;
   }
-  .brandmanagement .top-span-bot{
+  .brandmanagement-content .top-span-bot{
     color: #A0A0A0;
     font-size: 10px;
     line-height: 40px;
   }
-  .brandmanagement .el-button{
+  .brandmanagement-content .el-button{
     padding: 5px 10px;
     font-size: 10px;
     background-color: #ED6C2E;
