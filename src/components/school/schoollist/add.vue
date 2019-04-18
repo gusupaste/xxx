9b44fe1b2ab1826a6eeb7f843e1c8446cfd1dfa5
huangxuejing <huangxuejing@xingaokaowang.com>
2019-04-18 09:56:27 +0800

    a

--- src/components/school/schoollist/add.vue
+++ src/components/school/schoollist/add.vue
@@ -4,7 +4,28 @@
       <p>YOU ARE HERE : 校园 >> 校园列表 >> <span class="font-cl-blue">新增校园</span></p>
       <p class="bold">请输入新校园的相关系统,并保存提交,带*号的为必填字段。</p>
       <div class="body-content">
-        <div class="bg-dark-blue vertical-bar"></div><span class="font-cl-blue margin-left-10">基础信息</span>
+        <div class="vertical-bar">基础信息</div>
+        <hr class="line-solid">
+        <el-form :inline="true" :label-position="labelPosition" label-width="120px" :model="formInline" class="demo-form-inline ">
+          <el-form-item label="校园名称(中文):">
+            <el-input v-model="formInline.user" placeholder="请输入"></el-input>
+          </el-form-item>
+          <el-form-item label="简称(中文):">
+            <el-input v-model="formInline.user" placeholder="请输入"></el-input>
+          </el-form-item>
+          <el-form-item label="校园名称(英文):">
+            <el-input v-model="formInline.user" placeholder="请输入"></el-input>
+          </el-form-item>
+          <el-form-item label="简称(英文):">
+            <el-input v-model="formInline.user" placeholder="请输入"></el-input>
+          </el-form-item>
+          <el-form-item label="简称(中文):">
+            <el-input v-model="formInline.user" placeholder="请输入"></el-input>
+          </el-form-item>
+          <el-form-item label="简称(中文):">
+            <el-input v-model="formInline.user" placeholder="请输入"></el-input>
+          </el-form-item>
+        </el-form>
       </div>
     </div>
 
@@ -13,7 +34,15 @@
 
 <script>
   export default {
-    name: 'add'
+    data() {
+      return {
+        labelPosition: 'right',
+        formInline: {
+          user: '',
+          region: ''
+        }
+      }
+    }
   }
 </script>
 
@@ -44,11 +73,24 @@
     text-align: left;
   }
   .schooladd .vertical-bar{
-    width: 5px;
-    height: 12px;
-    display: inline-block;
-  }
-  .schooladd .margin-left-10{
+    width: 65px;
+    height: 16px;
+    color: #0b6289;
+    text-align: right;
     margin-left: 10px;
+    border-left: 4px solid #0b6289;
+  }
+  .schooladd .line-solid{
+    width: 100%;
+    text-align: center;
+    margin: 10px 0;
+    color: #ccc;
+  }
+  .schooladd .el-input__inner{
+    width: 250px;
+    height: 36px;
+  }
+  .schooladd .demo-form-inline{
+    text-align: center;
   }
 </style>
