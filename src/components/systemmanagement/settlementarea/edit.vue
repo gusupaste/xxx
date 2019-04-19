a8a2c150a4788b5f0351808fb0e3a2aa2a894f97
mn <1076660242@qq.com>
2019-04-19 15:19:21 +0800

    commit

--- src/components/systemmanagement/settlementarea/edit.vue
+++ src/components/systemmanagement/settlementarea/edit.vue
@@ -3,6 +3,19 @@
     <div class="header">
       <p>YOU ARE HERE : 系统管理 >> <span>品牌管理</span></p>
     </div>
+    <div class="settlementedit-content">
+      <el-form ref="settlementForm" :model="settlementForm" label-width="80px">
+        <el-form-item label="模版名称">
+          <el-input v-model="settlementForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="学年">
+          <el-select v-model="settlementForm.year">
+            <el-option v-for="year in selectYear" :label="year.year" value="year.id"></el-option>
+            <el-option ></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -11,9 +24,16 @@
     data() {
       return {
         labelPosition: 'right',
-        formInline: {
-          user: '',
-          region: ''
+        selectYear:[{
+          id:1,
+          year:2018,
+        },{
+          id:2,
+          year:2019,
+        }],
+        settlementForm: {
+          name: '',
+          year: ''
         }
       }
     }
@@ -23,11 +43,12 @@
 <style>
   .settlementedit .header p{
     font-size: 14px;
-    margin: 10px 0px;
   }
   .settlementedit{
-    padding: 0px 40px;
     color: rgba(160, 160, 160, 1);
     text-align: left;
+  }
+  .settlementedit .settlementedit-content{
+    margin-top: 20px;
   }
 </style>
