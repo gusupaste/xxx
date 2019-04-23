7f3a2a9519544d7e4c74176ea920add1d90ccd2b
gusupaste <632016865@qq.com>
2019-04-23 08:54:26 +0800

    update

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/var/lib/jenkins/jobs/etonkids-web/workspace/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
