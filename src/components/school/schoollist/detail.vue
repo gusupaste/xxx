730df5f47061e584b4bec8efda04ac4e4e3c6905
gusupaste <632016865@qq.com>
2019-04-24 15:13:02 +0800

    update

--- src/components/school/schoollist/detail.vue
+++ src/components/school/schoollist/detail.vue
@@ -5,25 +5,25 @@
     </div>
     <div class="content clearfix">
       <div class="content-top">北京理都校园</div>
-      <el-col :span="24">
+      <el-col :span="24" style="line-height:2">
         <el-card shadow="always">
           <p class="bold">基本信息</p>
-          <p class="bold">校园名称:北京首府校园(英文全称:xxxxxxxx;中文简称:xxxxxxxx;英文简称:xxxxxxxxxx)</p>
+          <p class="bold">校园名称：北京首府校园(英文全称:xxxxxxxx;中文简称:xxxxxxxx;英文简称:xxxxxxxxxx)</p>
           <p><el-row :gutter="20">
-            <el-col :span="4"><div class="grid-content bg-purple"><span>所属区域:</span><span>华北地区</span></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"><span>所属城市:</span><span>北京市</span></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"><span class="content-margin">园长:</span><span>李园长</span></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"><span>校园编码:</span><span>1001</span></div></el-col>
+            <el-col :span="4"><div class="grid-content bg-purple"><span>所属区域：</span><span>华北地区</span></div></el-col>
+            <el-col :span="4"><div class="grid-content bg-purple"><span>所属城市：</span><span>北京市</span></div></el-col>
+            <el-col :span="4"><div class="grid-content bg-purple"><span class="content-margin">园长：</span><span>李园长</span></div></el-col>
+            <el-col :span="4"><div class="grid-content bg-purple"><span>校园编码：</span><span>1001</span></div></el-col>
           </el-row>
           </p>
           <p>
           <el-row :gutter="20">
-            <el-col :span="4"><div class="grid-content bg-purple"><span>通讯地址:</span><span>北京市</span></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"><span class="content-margin">邮编:</span><span>123456</span></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"><span class="content-margin">电话:</span><span>18809876543</span></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"><span class="content-margin">传真:</span><span>xxxxxxxx</span></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"><span>电子邮箱:</span><span>xxxxxxxx</span></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"><span class="content-margin">网址:</span><span>www.taidii.com</span></div></el-col>
+            <el-col :span="4"><div class="grid-content bg-purple"><span>通讯地址：</span><span>北京市</span></div></el-col>
+            <el-col :span="4"><div class="grid-content bg-purple"><span class="content-margin">邮编：</span><span>123456</span></div></el-col>
+            <el-col :span="4"><div class="grid-content bg-purple"><span class="content-margin">电话：</span><span>18809876543</span></div></el-col>
+            <el-col :span="4"><div class="grid-content bg-purple"><span class="content-margin">传真：</span><span>xxxxxxxx</span></div></el-col>
+            <el-col :span="4"><div class="grid-content bg-purple"><span>电子邮箱：</span><span>xxxxxxxx</span></div></el-col>
+            <el-col :span="4"><div class="grid-content bg-purple"><span class="content-margin">网址：</span><span>www.taidii.com</span></div></el-col>
           </el-row>
           </p>
         </el-card>
@@ -83,7 +83,8 @@
                 <el-table-column
                   label="操作">
                   <template slot-scope="scope">
-                    <el-button type="text" size="small">编辑</el-button>
+                    <i class="fa fa-pencil green font-size-20 cur" @click="editClassDialog=true"></i>
+                    <i class="fa fa-trash red font-size-20 ml10 cur" @click="deleteDialog=true"></i>
                   </template>
                 </el-table-column>
               </el-table>
@@ -169,12 +170,94 @@
           </el-tab-pane>
         </el-tabs>
     </div>
+    <!-- 新增班级 -->
     <el-dialog title="新增班级" :visible.sync="dialogFormVisible"
-      width="70%"
-    >
+      width="70%">
       <el-form :model="form" :inline="true">
         <el-form-item label="新增方式：" :label-width="formLabelWidth">
-          <el-input v-model="form.name" auto-complete="off"></el-input>
+          <el-select v-model="form.name" auto-complete="off" class="w250_input">
+              <el-option value="手工新增">手工新增</el-option>
+              <el-option value="根据批复的开班申请创建">根据批复的开班申请创建</el-option>
+          </el-select>
+        </el-form-item>
+        <br>
+        <el-form-item label="中文名称：" :label-width="formLabelWidth">
+            <el-input v-model="form.region" placeholder="请输入"></el-input>
+          </el-form-item>
+          <el-form-item label="昵称：" :label-width="formLabelWidth">
+           <el-input v-model="form.region" placeholder="请输入"></el-input>
+          </el-form-item>
+          <el-form-item label="英文名称：" :label-width="formLabelWidth">
+            <el-input v-model="form.region" placeholder="请输入"></el-input>
+          </el-form-item>
+          <br>
+          <el-form-item label="班级类型：" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="请选择活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="年级：" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="请选择活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <br>
+          <el-form-item label="开设时间：" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="请选择活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="班级状态：" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="请选择活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <br>
+          <el-form-item label="满班学生数：" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="请选择活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="师资标配数：" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="请选择活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <br>
+          <el-form-item label="是否需要家具：" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="请选择活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="是否需要教具：" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="请选择活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <br>
+          <el-form-item label="班级描述：" :label-width="formLabelWidth" style="width:100%">
+            <el-input type="textarea" v-model="form.desc" style="min-width:600px"></el-input>
+          </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer" style="margin-top:0">
+          <el-button @click="dialogFormVisible=false" style="background-color:#bbb;color:#fff" >取 消</el-button>
+          <el-button type="primary" class="white bg-green" style="border-color:#8bc34a" >保 存</el-button>
+      </div>
+    </el-dialog>
+    <!-- 编辑班级 -->
+    <el-dialog title="编辑班级" :visible.sync="editClassDialog"
+      width="70%">
+      <el-form :model="form" :inline="true">
+        <el-form-item label="" :label-width="formLabelWidth">
+          
         </el-form-item>
         <br>
         <el-form-item label="中文名称：" :label-width="formLabelWidth">
@@ -253,9 +336,26 @@
           </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer" style="margin-top:0">
-          <el-button @click="dialogFormVisible=false" style="background-color:#bbb;color:#fff">取 消</el-button>
-          <el-button type="primary" style="background-color:#8bc34a;color:#fff;border-color:#8bc34a">保 存</el-button>
+          <el-button @click="editClassDialog=false" style="background-color:#bbb;color:#fff">取 消</el-button>
+          <el-button type="primary" class="white bg-green" style="border-color:#8bc34a">保 存</el-button>
       </div>
+    </el-dialog>
+    <!-- 删除班级 -->
+    <el-dialog
+      title="删除班级"
+      :visible.sync="deleteDialog"
+      width="30%"
+      :before-close="handleClose">
+      <div class="text-align-center">
+        是否确定删除班级
+        <span class="udline font-cl-blue">【双语大一班】？</span>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <div slot="footer" class="dialog-footer mt26">
+          <el-button @click="deleteDialog=false" style="background-color:#bbb;color:#fff">取 消</el-button>
+          <el-button type="primary" class="white bg-green" style="border-color:#8bc34a">保 存</el-button>
+      </div>
+      </span>
     </el-dialog>
   </div>
 </template>
@@ -297,9 +397,6 @@
       color:#3683a0;
       border: 2px solid #3683a0;
       border-radius: 20px;
-    }
-    .schooldetail .content {
-      /* padding: 20px 70px 50px 70px; */
     }
     .schooldetail .icon-font {
       color:#f17128;
@@ -348,6 +445,13 @@
     .schooldetail .content-margin{
       margin-left: 15%;
     }
+    .schooldetail >>> td , .schooldetail >>> th {
+      text-align: center;
+    }
+    .schooldetail >>> .el-textarea__inner {
+        width: 600px;
+        min-height: 100px !important;
+    }
   </style>
 
 <script>
@@ -357,9 +461,11 @@
     data() {
       return {
         dialogFormVisible:false,
+        editClassDialog:false,
+        deleteDialog:false,
         activeName: 'first',
         form: {
-          name: '',
+          name: '手工新增',
           region: '',
           date1: '',
           date2: '',
@@ -418,6 +524,13 @@
       },
       viewSchoolCalendar(){
         this.$router.push('/school/SchoolViewClendar/1')
+      },
+      handleClose(done) {
+        this.$confirm('确认关闭？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {});
       }
     }
   }
