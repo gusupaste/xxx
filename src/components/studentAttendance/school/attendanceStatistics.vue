ddeb29310e9f54a5b53a8ea092294f67be93d642
huangxuejing <huangxuejing@xingaokaowang.com>
2019-04-25 14:20:19 +0800

    d

--- src/components/studentAttendance/school/attendanceStatistics.vue
+++ src/components/studentAttendance/school/attendanceStatistics.vue
@@ -3,14 +3,44 @@
     <div class="header">
       <p class="local_path_style">YOU ARE HERE : > 学生考勤<span class="font-cl-blue">考勤统计</span></p>
     </div>
+    <p class="mt10">
+      <span>学年：</span>
+      <el-select v-model="value" placeholder="请选择">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+      <span>月份：</span>
+      <el-select v-model="value" placeholder="请选择">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+      <span>班级：</span>
+      <el-select v-model="value" placeholder="请选择">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+      <span class="padding-left-30"><el-button type="primary">搜索</el-button></span>
+     </p>
     <el-form class="mt10" label-width="80px" :model="formLabelAlign" inline="">
-      <el-form-item label="名称">
+      <el-form-item label="学年">
         <el-input v-model="formLabelAlign.name"></el-input>
       </el-form-item>
-      <el-form-item label="活动区域">
+      <el-form-item label="月份">
         <el-input v-model="formLabelAlign.region"></el-input>
       </el-form-item>
-      <el-form-item label="活动形式">
+      <el-form-item label="班级">
         <el-input v-model="formLabelAlign.type"></el-input>
       </el-form-item>
       <el-form-item>
@@ -25,18 +55,26 @@
 </style>
 <script>
   export default {
-    data(){
+    data () {
       return {
+        value: '-所有-',
+         options: [{
+          value: '123',
+          label: '333'
+        },{
+          value: '456',
+          label: '666'
+        }],
         formLabelAlign: {
-          name: '',
+          year: '2018-01',
           region: '',
           type: ''
         }
       }
     },
-    methods:{
-      onSubmit() {
-        console.log('submit!');
+    methods: {
+      onSubmit () {
+        console.log('submit!')
       }
     }
   }
