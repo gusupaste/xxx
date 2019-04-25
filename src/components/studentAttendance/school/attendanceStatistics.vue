11161326b6e082edc2c96c5218115e32698482fd
huangxuejing <huangxuejing@xingaokaowang.com>
2019-04-25 16:07:07 +0800

    Auto stash before rebase of "origin/feature/eltonkids_development"

--- src/components/studentAttendance/school/attendanceStatistics.vue
+++ src/components/studentAttendance/school/attendanceStatistics.vue
@@ -33,20 +33,52 @@
       </el-select>
       <span class="padding-left-30"><el-button type="primary">搜索</el-button></span>
      </p>
-    <el-form class="mt10" label-width="80px" :model="formLabelAlign" inline="">
-      <el-form-item label="学年">
-        <el-input v-model="formLabelAlign.name"></el-input>
-      </el-form-item>
-      <el-form-item label="月份">
-        <el-input v-model="formLabelAlign.region"></el-input>
-      </el-form-item>
-      <el-form-item label="班级">
-        <el-input v-model="formLabelAlign.type"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">搜索</el-button>
-      </el-form-item>
-    </el-form>
+    <template>
+      <el-table
+        class="mt26"
+        :data="tableData"
+        border
+        stripe
+        show-header
+        style="width: 100%">
+        <el-table-column
+          prop="code"
+          label="学号"
+          min-width="30">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="学生姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="discount_type"
+          label="班级">
+        </el-table-column>
+        <el-table-column
+          prop="discount_budget"
+          label="应出勤天数">
+        </el-table-column>
+        <el-table-column
+          prop="discount_budget"
+          label="实际出勤天数">
+        </el-table-column><el-table-column
+        prop="discount_budget"
+        label="事假">
+      </el-table-column><el-table-column
+        prop="discount_budget"
+        label="病假">
+      </el-table-column>
+        <el-table-column
+          prop="discount_budget"
+          label="休学">
+        </el-table-column>
+        <el-table-column
+          prop="出勤率"
+          label="应出勤天数">
+        </el-table-column>
+      </el-table>
+    </template>
   </div>
 </template>
 <style scoped>
