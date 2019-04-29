923336fdf0568f6786acab549edeed31413c6a26
gusupaste <632016865@qq.com>
2019-04-29 20:15:32 +0800

    update

--- src/components/school/intercitylist/list.vue
+++ src/components/school/intercitylist/list.vue
@@ -22,15 +22,15 @@
               </div>
               <div class="left" style="width:15%">
                   <el-button type="text" @click="editIntercity(item)">
-                  <i class="fa fa-edit icon-font"></i>
+                  <i class="fa fa-edit icon-font cur"></i>
                 </el-button>
               </div>
             </div>
-            <draggable class="list-group" :list="list1" group="people" @change="log">
+            <draggable class="list-group" :list="item.center_list" group="people" @change="log">
               <div class="list-group-item intercity-li"
-                   v-for="(element, index) in list1"
-                   :key="element.name">
-                {{ element.name }} {{ index }}
+                   v-for="s in item.center_list"
+                   :key="s.id">
+                {{ s.name }}
               </div>
             </draggable>
           </el-card>
@@ -47,7 +47,7 @@
           </el-form-item>
           <el-form-item label="负责人：" prop="person">
             <el-input v-model="form.person.name" disabled style="width:164px"></el-input>
-            <el-button type="primary" @click="addinnerVisible = true">
+            <el-button type="primary" @click="addinnerVisible = true" style="height:40px">
               <i class="fa fa-search"></i>
             </el-button>
           </el-form-item>
@@ -59,7 +59,7 @@
       </el-dialog>
       <!-- 编辑城际 -->
       <el-dialog title="编辑城际" :visible.sync="editintercityVisible" width="80%" >
-        <el-form ref="form" :model="checkedItem" :rules="rules" label-width="100px">
+        <el-form  :model="checkedItem" :rules="rules2" ref="checkedItem" label-width="100px">
           <el-form-item label="城际名称：" prop="name">
             <el-input v-model="checkedItem.dept_name" class="w250_input" style="width:250px"></el-input>
             <span class="cur red ml10" @click="deleteDialog=true">删除此城际？</span>
@@ -92,7 +92,7 @@
         </el-form>
         <span slot="footer" class="dialog-footer">
           <el-button @click="editintercityVisible = false" class="bg-grey white">取 消</el-button>
-          <el-button type="success" @click="editintercityVisible = false">保 存</el-button>
+          <el-button type="success" @click="sureEditIntercity">保 存</el-button>
         </span>
       </el-dialog>
       <!-- 删除城际 -->
@@ -111,6 +111,7 @@
       <!-- 选择负责人 -->
       <el-dialog
           width="30%"
+          class="chose-person"
           title="选择负责人"
           :visible.sync="addinnerVisible"
           append-to-body>
@@ -129,7 +130,7 @@
               label="选择"
               width="100">
               <template slot-scope="scope">
-                <el-radio v-model="choosePerson" @change="checkedPerson(scope.row)"></el-radio>
+                <el-radio v-model="choosePerson" :label="scope.row"> </el-radio>
               </template>
             </el-table-column>
             <el-table-column
@@ -150,253 +151,21 @@
             :current-page="currentPage"
             :total="count">
           </el-pagination>
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="addinnerVisible = false" class="bg-grey white">取 消</el-button>
+            <el-button type="success" @click="checkedPerson">确 定</el-button>
+        </span>
         </el-dialog>
     </div>
 </template>
-
-<script>
-  import draggable from 'vuedraggable';
-  export default {
-    components: {
-      draggable
-    },
-    data() {
-      const generateData = _ => {
-        const data = [];
-        for (let i = 1; i <= 15; i++) {
-          data.push({
-            key: i,
-            label: `备选项 ${ i }`,
-            disabled: i % 4 === 0
-          });
-        }
-        return data;
-      };
-      return {
-        searchPerson:"",
-        personTable:[],
-        choosePerson:Number,
-        /**分页数据 */
-        count:1,
-        currentPage:1,
-        pagesize:1,
-        data: generateData(),
-        value1: [1, 4],
-        /**弹窗参数 */
-        addintercityVisible : false,
-        addinnerVisible : false,
-        editintercityVisible : false,
-        deleteDialog:false,
-        balls: [
-          {
-            id:1,
-            name:'haha1',
-          },
-          {
-            id:2,
-            name:'haha2',
-          }
-        ],
-        form: {
-          name: '',
-          code: '',
-          person:{},
-        },
-        checkedItem:{},
-        intercityList:[],
-        schoolType:[],
-        rules: {
-          name: [
-            {required: true, message: '请输入城际名称', trigger: 'blur'},
-            {min:1, max: 50, message: '长度在 1 到 50 个字符', trigger: 'blur'}
-          ],
-          code: [
-            {required: true, message: '请输入城际代码', trigger: 'blur'},
-            {min: 1, max: 10, message: '长度在 1 到 10 个字符', trigger: 'blur'}
-          ],
-          person: [
-            {required: true, message: '请输入负责人名称', trigger: 'blur'},
-          ],
-        },
-        list1: [
-          { name: "John", id: 1 },
-          { name: "Joao", id: 2 },
-          { name: "Jean", id: 3 },
-          { name: "Gerard", id: 4 }
-        ],
-        list2: [
-          { name: "Juan", id: 5 },
-          { name: "Edgard", id: 6 },
-          { name: "Johnson", id: 7 }
-        ],
-        list2: [
-          { name: "Juan", id: 5 },
-          { name: "Edgard", id: 6 },
-          { name: "Johnson", id: 7 }
-        ],
-        selectSchool: [
-          {
-            name: "幼儿园1",
-            id: 1,
-          },
-          {
-            name: "幼儿园2",
-            id: 2
-          },
-          {
-            name: "幼儿园3",
-            id: 3
-          }
-        ],
-        UNSelectSchool: [],
-      };
-    },
-    created () {
-      this.getPerson();
-      this.getIntercity();
-    },
-    methods: {
-      addIntercity(formName){
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            var _this = this;
-            this.$axios.post('http://192.168.1.197:8000/api/common/intercity/',{
-                dept_name:_this.form.name,
-                dept_code:_this.form.code,
-                manager_id:_this.form.person.id,
-            }).then(res=>{
-              _this.$message({
-                type:'success',
-                message:'新增城际成功'
-              })
-              console.log(res.data);
-              _this.addintercityVisible = false;
-              _this.getIntercity()
-            }).catch(err=>{
-              _this.$message({
-                type:'error',
-                message:'新增失败'
-              });
-              _this.addintercityVisible = false;
-          })
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-          
-      },
-      editIntercity(val){
-        console.log(val)
-        // 
-        var _this = this;
-          this.$axios.get('http://192.168.1.197:8000/api/common/intercity/'+val.id+'/view_detail/',)
-          .then(res=>{
-            _this.checkedItem = res.data.detail;
-            console.log(res)
-          }).catch(err=>{
-
-        })
-        this.editintercityVisible = true;
-      },
-      deleteIntercity(){
-          var _this = this;
-          this.$axios.delete('http://192.168.1.197:8000/api/common/intercity/'+this.checkedItem.id+'/',)
-          .then(res=>{
-            _this.$message({
-                type:'success',
-                message:'删除城际成功'
-            })
-            _this.getIntercity();
-          }).catch(err=>{
-            _this.$message({
-                type:'error',
-                message:'删除城际失败'
-            })
-        })
-        this.editintercityVisible = false;
-        this.deleteDialog = false;
-      },
-      getIntercity(){
-          var _this = this;
-          this.$axios.get('http://192.168.1.197:8000/api/common/intercity/',).then(res=>{
-            _this.intercityList = res.data.intercity_list;
-            _this.addintercityVisible = false;
-          }).catch(err=>{
-          console.log(err)
-        })
-      },
-      getPerson(){
-          var _this = this;
-          this.$axios.get('http://192.168.1.197:8000/api/common/select/user_list/',{
-            params:{
-              user_name:_this.searchPerson,
-              page:_this.currentPage,
-              size:10
-            }
-          }).then(res=>{
-            _this.personTable = res.data.results.results;
-            _this.count = res.data.results.page_number;
-            // _this.addintercityVisible = false;
-          }).catch(err=>{
-          console.log(err)
-        })
-      },
-      changeSchool(val){
-        console.log(val)
-      },
-      handleCurrentChange(val) {
-        this.currentPage = val;
-        console.log(this.currentPage)
-      },
-      checkedPerson(val) {
-        this.form.person = val;
-      },
-      add: function() {
-        this.list.push({ name: "Juan" });
-      },
-      replace: function() {
-        this.list = [{ name: "Edgard" }];
-      },
-      clone: function(el) {
-        return {
-          name: el.name + " cloned"
-        };
-      },
-      log: function(evt) {
-        window.console.log(evt);
-      },
-      selectFun: function(obj) {
-        console.log(obj);
-        var index = this.UNSelectSchool.indexOf(obj);
-        this.UNSelectSchool.splice(index, 1);
-      }
-    },
-    watch: {
-      currentPage:function(){
-        this.getPerson();
-      }
-    }
-  }
-</script>
 
 <style scoped>
   .intercitylist{
     color: rgba(160, 160, 160, 1);
     text-align: left;
   }
-  .intercitylist >>> .el-dialog__header{
-        background-color: #f5f5f5;
-        padding: 15px;
-        font-size: 12px !important;
-        border-bottom: 1px solid #d5d5d5;
-    }
-  .intercitylist >>> .el-dialog__title{
-        font-size: 14px;
-    }
-  .intercitylist .bold{
-    font-weight: bold;
-    color: black;
+  .chose-person >>> .el-radio__label{
+    display: none !important;
   }
   .intercitylist .intercity-list{
     width: 210px;
@@ -411,6 +180,9 @@
     font-weight: 600;
     display: block;
     font-size: 16px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
   }
   .intercitylist .intercity-li{
     border: 1px solid #ddd;
@@ -474,4 +246,230 @@
   .intercitylist >>> .el-table td{
     padding: 5px 0 !important;
   }
+  
 </style>
+
+<script>
+  import draggable from 'vuedraggable';
+  export default {
+    components: {
+      draggable
+    },
+    data() {
+      return {
+        searchPerson:"",
+        personTable:[],
+        choosePerson:Number,
+        /**分页数据 */
+        count:1,
+        currentPage:1,
+        pagesize:1,
+        value1: [1, 4],
+        /**弹窗参数 */
+        addintercityVisible : false,
+        addinnerVisible : false,
+        editintercityVisible : false,
+        deleteDialog:false,
+        balls: [
+          {
+            id:1,
+            name:'haha1',
+          },
+          {
+            id:2,
+            name:'haha2',
+          }
+        ],
+        form: {
+          name: '',
+          code: '',
+          person:'',
+        },
+        checkedItem:{},
+        intercityList:[],
+        schoolType:[],
+        rules: {
+          name: [
+            {required: true, message: '请输入城际名称', trigger: 'blur'},
+            {min:1, max: 50, message: '长度在 1 到 50 个字符', trigger: 'blur'}
+          ],
+          code: [
+            {required: true, message: '请输入城际代码', trigger: 'blur'},
+            {min: 1, max: 10, message: '长度在 1 到 10 个字符', trigger: 'blur'},
+            
+          ],
+          person: [
+            {required: true, message: '请输入负责人名称', trigger: 'blur'},
+          ],
+        },
+        rules2: {
+          name: [
+            {required: true, message: '请输入城际名称', trigger: 'blur'},
+            {min:1, max: 50, message: '长度在 1 到 50 个字符', trigger: 'blur'}
+          ],
+          code: [
+            {required: true, message: '请输入城际代码', trigger: 'blur'},
+            {min: 1, max: 10, message: '长度在 1 到 10 个字符', trigger: 'blur'},
+            
+          ],
+          person: [
+            {required: true, message: '请输入负责人名称', trigger: 'blur'},
+          ],
+        },
+        selectSchool: [],
+        UNSelectSchool: [],
+      };
+    },
+    created () {
+      this.getPerson();
+      this.getIntercity();
+    },
+    methods: {
+      addIntercity(formName){
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            var _this = this;
+            this.$axios.post('http://192.168.1.197:8000/api/common/intercity/',{
+                dept_name:_this.form.name,
+                dept_code:_this.form.code,
+                manager_id:_this.form.person.id,
+            }).then(res=>{
+              _this.$message({
+                type:'success',
+                message:'新增城际成功'
+              })
+              _this.addintercityVisible = false;
+              _this.getIntercity()
+              this.$refs['form'].resetFields();
+            }).catch(err=>{
+              _this.$message({
+                type:'error',
+                message:'新增失败'
+              });
+          })
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+          
+      },
+      editIntercity(val){
+        console.log(val)
+        // 
+        var _this = this;
+          this.$axios.get('http://192.168.1.197:8000/api/common/intercity/'+val.id+'/view_detail/',)
+          .then(res=>{
+            _this.checkedItem = res.data.detail;
+            _this.UNSelectSchool = res.data.detail.center_list;
+            console.log(res)
+          }).catch(err=>{
+
+        })
+        this.editintercityVisible = true;
+      },
+      deleteIntercity(){
+          var _this = this;
+          this.$axios.delete('http://192.168.1.197:8000/api/common/intercity/'+this.checkedItem.id+'/',)
+          .then(res=>{
+            _this.$message({
+                type:'success',
+                message:'删除城际成功'
+            })
+            _this.getIntercity();
+          }).catch(err=>{
+            _this.$message({
+                type:'error',
+                message:'删除城际失败'
+            })
+        })
+        this.editintercityVisible = false;
+        this.deleteDialog = false;
+      },
+      getIntercity(){
+          var _this = this;
+          this.$axios.get('http://192.168.1.197:8000/api/common/intercity/',).then(res=>{
+            _this.intercityList = res.data.intercity_list;
+            _this.addintercityVisible = false;
+          }).catch(err=>{
+          console.log(err)
+        })
+      },
+      getPerson(){
+          var _this = this;
+          this.$axios.get('http://192.168.1.197:8000/api/common/select/user_list/',{
+            params:{
+              user_name:_this.searchPerson,
+              page:_this.currentPage,
+              size:10
+            }
+          }).then(res=>{
+            _this.personTable = res.data.results.results;
+            _this.count = res.data.results.page_number;
+            // _this.addintercityVisible = false;
+          }).catch(err=>{
+          console.log(err)
+        })
+      },
+      sureEditIntercity(){
+        var id = this.checkedItem.id;
+        var _this = this;
+        var list = [];
+        for (let i = 0; i < this.UNSelectSchool.length; i++) {
+          list.push(this.UNSelectSchool[i].id);
+        };
+          this.$axios.put('http://192.168.1.197:8000/api/common/intercity/'+id+'/',{
+              dept_name:_this.checkedItem.dept_name,
+              dept_code:_this.checkedItem.dept_code,
+              manager_id:_this.checkedItem.manager_id,
+              center_ids:list,
+          }).then(res=>{
+            _this.$message({
+              type:'success',
+              message:'编辑成功',
+            })
+            _this.getIntercity();
+            _this.editintercityVisible = false;
+          }).catch(err=>{
+            _this.$message({
+              type:'error',
+              message:'编辑失败',
+            })
+        })
+      },
+      changeSchool(val){
+        console.log(val)
+      },
+      handleCurrentChange(val) {
+        this.currentPage = val;
+      },
+      checkedPerson() {
+        this.form.person = this.choosePerson;
+        this.addinnerVisible = false;
+      },
+      add: function() {
+        this.list.push({ name: "Juan" });
+      },
+      replace: function() {
+        this.list = [{ name: "Edgard" }];
+      },
+      clone: function(el) {
+        return {
+          name: el.name + " cloned"
+        };
+      },
+      log: function(evt) {
+        window.console.log(evt);
+      },
+      selectFun: function(obj) {
+        var index = this.UNSelectSchool.indexOf(obj);
+        this.UNSelectSchool.splice(index, 1);
+      }
+    },
+    watch: {
+      currentPage:function(){
+        this.getPerson();
+      }
+    }
+  }
+</script>
