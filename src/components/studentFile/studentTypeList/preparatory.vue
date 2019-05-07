41779c5e99f114839a1f5b8acfd56f54515095c6
mn <1076660242@qq.com>
2019-05-07 09:15:41 +0800

    commit

--- src/components/studentFile/studentTypeList/preparatory.vue
+++ src/components/studentFile/studentTypeList/preparatory.vue
@@ -47,7 +47,7 @@
             </el-select>
           </el-form-item>
           <el-form-item label="校园：">
-            <el-select v-model="school">
+            <el-select v-model="school" @change="schoolChangeFun">
               <el-option value="" label="全部"></el-option>
               <el-option
                 v-for="item in school_list"
@@ -58,10 +58,10 @@
             </el-select>
           </el-form-item>
           <el-form-item label="班级：">
-            <el-select v-model="klass">
+            <el-select v-model="class_val" v-bind:disabled="selectDisable">
               <el-option value="" label="全部"></el-option>
               <el-option
-                v-for="item in school_list"
+                v-for="item in class_list"
                 :key="item.id"
                 :label="item.name"
                 :value="item.id">
@@ -88,26 +88,27 @@
             <el-input placeholder="输入学号、学生姓名或者学生卡号" class="w250_input"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" >搜索</el-button>
+            <el-button type="primary" @click="getStudentList">搜索</el-button>
           </el-form-item>
         </el-form>
 
       </div>
       <div class="studentFileList">
-          <div class="studentFileCard left" v-for="(item , index) in list " :key="index" @click="$router.push('/studentFile/studentFileDetail/9')">
+          <div class="studentFileCard left" v-for="(item,index) in student_list " :key="index" @click="$router.push('/studentFile/studentFileDetail/9')">
             <div style="padding:30px 20px">
               <div class="avatar inline-block">
                 <img src="../../../assets/img/logo.png" alt="">
               </div>
               <div class="card-content inline-block">
                   <p>
-                    <span style="font-size:15px;font-weight:600">学生A</span>
+                    <span style="font-size:15px;font-weight:600">{{ item.name }}</span>
                     <i style="font-size:15px;color:#ff7f7f" class="fa fa-times-circle"></i>
                   </p>
-                  <p>出生日期：1020/11/09</p>
-                  <p>意向学生：00000</p>
-                  <p>意向信息：北京丽都校园 双语小二班 </p>
-                  <p>拟入学日期：2019/04/01</p>
+                  <p>学号：{{ item.student_no }}</p>
+                  <p>出生日期：{{ item.date_of_birth }}</p>
+                  <p>意向学年：{{ item.preferred_academic_year }}</p>
+                  <p>意向信息：{{ item.preferred_info }}</p>
+                  <p>拟入学日期：{{ item.preferred_admission_date }}</p>
               </div>
             </div>
           </div>
@@ -183,6 +184,8 @@ export default {
       city_url:'http://192.168.199.157:8000/api/common/select/city_list/',/*省市*/
       brand_url:'http://192.168.199.157:8000/api/common/select/hq_list/',/*品牌*/
       school_url:'http://192.168.199.157:8000/api/common/select/center_list/',/*校园*/
+      klass_url:'http://192.168.199.157:8000/api/common/select/class_list/?center_id=',/*班级*/
+      student_url:'http://192.168.199.157:8000/api/student/student/student_list/',/*学生*/
       intercity_list:[],
       intercity:'',
       area_list:[],
@@ -193,9 +196,13 @@ export default {
       brand:'',
       school_list:[],
       school:'',
+      class_list:[],
+      class_val:'',
       klass:'',
       gender:'',
       dateValue: '',
+      student_list:[],
+      selectDisable:'',
       list:[1,2,3,4,5,6,7,81,],
       form: {
       name: '',
@@ -215,6 +222,7 @@ export default {
     this.getCityList(0);
     this.getBrandList();
     this.getSchoolList();
+    this.getStudentList();
   },
   methods:{
     /*城际*/
@@ -287,6 +295,42 @@ export default {
         console.log(err)
       })
     },
+    /*班级*/
+    getClassList:function () {
+      var _this = this;
+      var url = this.klass_url + this.school;
+      _this.$axios.get(url).then(res=>{
+        _this.loading = false;
+        if(res.status == 200 && res.data.status_code == 1) {
+          this.class_list = res.data.results;
+        }
+      }).catch(err=>{
+        console.log(err)
+      })
+    },
+    /*获取学生*/
+    getStudentList:function () {
+      console.log(this.dateValue);
+      var _this = this;
+      var url = this.student_url;
+      var school_ids = [3];
+      _this.$axios.post(url,
+      {
+        student_type:'Prepare',
+        center_ids:school_ids,
+        class_id:this.class_val,
+        date_from:'2018-01-01',
+        date_to:'2020-01-01',
+        gender:"",
+        condition:"",
+      }).then(res=>{
+        if(res.status == 200 && res.data.status == 1) {
+          this.student_list = res.data.results.results;
+        }
+      }).catch(err=>{
+        console.log(err)
+      })
+    },
     areaChangeFun:function () {
       this.getCityList(this.area);
       this.getSchoolList(this.intercity,this.city,0,this.brand);
@@ -294,6 +338,15 @@ export default {
     /*intercity_id 城际，province_id 省份，area_code 区域code,hq_id 品牌*/
     allChangeFun:function () {
       this.getSchoolList(this.intercity,this.city,this.area,this.brand);
+    },
+    schoolChangeFun:function () {
+      console.log(this.school);
+      if(this.school === ''){
+        this.selectDisable = true;
+      }else{
+        this.getClassList();
+        this.selectDisable = false;
+      }
     },
   },
 }
