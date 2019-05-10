37a8aa6ee81fe7526f08d32216df3576e76cd6af
gusupaste <632016865@qq.com>
2019-05-10 09:17:44 +0800

    update

--- src/main.js
+++ src/main.js
@@ -30,7 +30,7 @@ axios.interceptors.request.use(
   config => {
     // config.baseURL = 'http://134.175.93.59:8000';
     // config.baseURL = 'http://192.168.1.197:8000';
-    config.baseURL = 'http://192.168.199.157:8000';
+    // config.baseURL = 'http://192.168.199.157:8000';
     // config.baseURL = 'http://etonkids.taidii.cn/';
     config.headers.post['Content-Type'] = 'application/json;utf-8';
     config.headers.common['Authorization'] = "jwt "+VueCookies.get('token') || '';
