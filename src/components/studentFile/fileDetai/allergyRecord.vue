0e6e36b3962e52e2a640da5fec14ad3e09c3fe65
mn <1076660242@qq.com>
2019-05-16 16:30:59 +0800

    commit

--- src/components/studentFile/fileDetai/allergyRecord.vue
+++ src/components/studentFile/fileDetai/allergyRecord.vue
@@ -23,7 +23,7 @@
            <el-table-column
              label="操作">
              <template slot-scope="scope">
-               <el-button type="text"class="red" @click="editAll(scope.row)" v-bind:disabled="ruleFormShow === true">
+               <el-button type="text"class="red" @click="editAll(scope.row)">
                  <i class="fa fa-pencil green cur"></i>
                </el-button>
                <el-button type="text"class="red" @click="deleteAll(scope.row)">
