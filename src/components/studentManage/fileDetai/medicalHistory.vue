5df5abd1609b3de8d7951e599b72d482b44e6300
mn <1076660242@qq.com>
2019-05-17 14:14:56 +0800

    commit

--- src/components/studentManage/fileDetai/medicalHistory.vue
+++ src/components/studentManage/fileDetai/medicalHistory.vue
@@ -32,7 +32,7 @@
                 <el-button type="text"class="red" @click="editMed(scope.row)">
                   <i class="fa fa-pencil green cur"></i>
                 </el-button>
-                <el-button type="text"class="red" @click="deleteMed(scope.row)">
+                <el-button type="text"class="red" @click="deleteVisible = true;deleteId = scope.row.id">
                   <i class="fa fa-trash red cur"></i>
                 </el-button>
                 <!--<i class="fa fa-pencil green font-size-20 cur" @click="editMed(scope.row)"></i>
@@ -71,6 +71,13 @@
             <button class="btn bg-grey mr26" @click="cancelMed">取消</button>
             <button class="btn bg-green" @click="submitForm('ruleForm')">保存</button>
         </div>
+      <el-dialog title="确认删除" :visible.sync="deleteVisible" width="400px">
+        <p class="mt26 text-align-center">确认删除该条记录？</p>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="deleteVisible = false">取 消</el-button>
+          <el-button type="success" @click="deleteMed">确 定</el-button>
+        </span>
+      </el-dialog>
     </div>
 </template>
 <style scoped>
@@ -88,6 +95,8 @@ export default {
     },
     data(){
         return {
+            deleteId:'',
+            deleteVisible:false,
             ruleFormShow:false,
             tableList: [],
             medica_options:[
@@ -177,14 +186,15 @@ export default {
         this.ruleForm.doctor_name = '';
         this.ruleForm.doctor_phone = '';
       },
-      deleteMed:function(obj){
+      deleteMed:function(){
           this.ruleFormShow = false;
-          this.$axios.delete(this.post_url + obj.id +'/').then(res=>{
+          this.$axios.delete(this.post_url + this.deleteId +'/').then(res=>{
             if(res.status == 200){
               this.$message({
                 type:'success',
                 message:'删除成功！'
               })
+              this.deleteVisible = false;
               this.getList();
             }else{
               this.$message.error('删除失败');
