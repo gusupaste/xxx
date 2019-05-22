aa2cc438d6c65b710dacf33fc943e6aa3a33fb88
mn <1076660242@qq.com>
2019-05-22 11:56:25 +0800

    commit

--- src/components/studentFile/fileDetai/basicInfo.vue
+++ src/components/studentFile/fileDetai/basicInfo.vue
@@ -48,15 +48,15 @@
                         <el-input v-model="studentInfo.certificate_no" placeholder="请填写证件号" class="w250_input"></el-input>
                     </el-form-item>
                     <br>
-                    <el-form-item label="属性：" prop="type"  label-width="150px">
-                        <el-checkbox-group v-model="studentInfo.type">
-                        <el-checkbox label="转园生" name="type"></el-checkbox>
-                        <el-checkbox label="业主" name="type"></el-checkbox>
-                        <el-checkbox label="享受兄弟姐妹折扣" name="type"></el-checkbox>
+                    <el-form-item label="属性：" label-width="150px">
+                        <el-checkbox-group v-model="newType">
+                        <el-checkbox label="is_employee_child" name="type">转园生</el-checkbox>
+                        <el-checkbox label="is_owner" name="type">业主</el-checkbox>
+                        <el-checkbox label="has_siblings_in_eton" name="type">享受兄弟姐妹折扣</el-checkbox>
                         </el-checkbox-group>
                     </el-form-item>
                     <br>
-                    <el-form-item label="国籍：" prop="region" label-width="150px" class="w250_input">
+                    <el-form-item label="国籍：" label-width="150px" class="w250_input">
                         <el-select v-model="studentInfo.nationality" placeholder="请选择国籍">
                           <el-option
                             v-for="item in Country_options"
@@ -66,7 +66,7 @@
                           </el-option>
                         </el-select>
                     </el-form-item>
-                    <el-form-item label="宗教信仰：" prop="region" label-width="150px" class="w250_input">
+                    <el-form-item label="宗教信仰：" label-width="150px" class="w250_input">
                         <el-select v-model="studentInfo.religion" placeholder="请选择宗教信仰">
                           <el-option
                             v-for="item in Belief_options"
@@ -87,23 +87,24 @@
                         </el-select>
                     </el-form-item>
                     <br>
-                    <el-form-item label="意向入学信息：" prop="desc" label-width="150px" style="width:100%" class="speciality">
-                      <el-input v-model="studentInfo.name" placeholder="请填写姓名" class="w250_input"></el-input>
+                    <!--<el-form-item label="意向入学信息**：" prop="desc" label-width="150px" style="width:100%" class="speciality">
+                      <el-input v-model="studentInfo.name" placeholder="请填写意向入学信息" class="w250_input"></el-input>
+                    </el-form-item>-->
+                    <br>
+                    <el-form-item label="所在班级：" label-width="150px" class="enroll-info">
+                        <el-select v-model="preferred_grade_type_name" disabled="disabled"></el-select>
+                        <el-select v-model="preferred_class_type_name" disabled="disabled"></el-select>
+                        <el-select v-model="studentInfo.preferred_class" placeholder="">
+                          <el-option
+                            v-for="item in class_list"
+                            :key="item.id"
+                            :label="item.name"
+                            :value="item.id">
+                          </el-option>
+                        </el-select>
                     </el-form-item>
                     <br>
-                    <el-form-item label="所在班级：" prop="class_info" label-width="150px" class="enroll-info">
-                        <el-select v-model="studentInfo.region" prop="year" placeholder="" disabled="disabled">
-
-                        </el-select>
-                        <el-select v-model="studentInfo.region" prop="grade" placeholder="" disabled="disabled">
-
-                        </el-select>
-                        <el-select v-model="studentInfo.region" prop="klass" placeholder="">
-
-                        </el-select>
-                    </el-form-item>
-                    <br>
-                    <el-form-item label="拟入学学年：" prop="enter_date" label-width="150px" style="text-align:left">
+                    <!--<el-form-item label="拟入学学年**：" prop="enter_date" label-width="150px" style="text-align:left">
                       <el-select v-model="studentInfo.academic_year" placeholder="请选择学年">
                       <el-option value="" label="全部"></el-option>
                       <el-option
@@ -114,11 +115,11 @@
                       </el-option>
                     </el-select>
                     </el-form-item>
-                    <el-form-item label="拟入学日期：" prop="enter_date" label-width="150px" style="text-align:left">
+                    <el-form-item label="拟入学日期**：" prop="enter_date" label-width="150px" style="text-align:left">
                       <el-date-picker type="date" placeholder="选择拟入学日期" class="w250_input" v-model="studentInfo.enter_date" style="width: 100%;"></el-date-picker>
-                    </el-form-item>
+                    </el-form-item>-->
                     <br>
-                    <el-form-item label="学生第一语言：" prop="date1" label-width="150px" class="w250_input">
+                    <el-form-item label="学生第一语言：" label-width="150px" class="w250_input">
                           <el-select v-model="studentInfo.primary_language" placeholder="请选择学生第一语言">
                             <el-option
                               v-for="item in Language_options"
@@ -128,7 +129,7 @@
                             </el-option>
                           </el-select>
                     </el-form-item>
-                    <el-form-item label="学生第二语言：" prop="date1" label-width="150px" class="w250_input">
+                    <el-form-item label="学生第二语言：" label-width="150px" class="w250_input">
                             <el-select v-model="studentInfo.other_language" placeholder="请选择学生第二语言">
                               <el-option
                                 v-for="item in Language_options"
@@ -139,22 +140,22 @@
                             </el-select>
                     </el-form-item>
                     <br>
-                    <el-form-item label="特长：" prop="desc" label-width="150px" style="width:100%" class="speciality">
+                    <el-form-item label="特长：" label-width="150px" style="width:100%" class="speciality">
                         <el-input type="textarea" v-model="studentInfo.remark"></el-input>
                     </el-form-item>
                     <el-form-item label="家庭住址：" prop="address" label-width="150px" class="enroll-info">
-                      <el-select class="select-region" v-model="studentInfo.p_city_id" placeholder="省" @change="proinit(1)">
+                      <el-select class="select-region" v-model="p_city_id" placeholder="省" @change="proinit(1)">
                         <el-option v-for="pro in provinceList" :label="pro.city_name" :value="pro.city_id" :key="pro.id"></el-option>
                       </el-select>
                       <el-select class="select-region" v-model="c_city_id" placeholder="市" @change="proinit(2)">
                         <el-option v-for="city in cityList" :label="city.city_name" :value="city.city_id" :key="city.id"></el-option>
                       </el-select>
-                      <el-select class="select-region" v-model="town_id" placeholder="区">
+                      <el-select class="select-region" v-model="studentInfo.town" placeholder="区">
                         <el-option v-for="town in townList" :label="town.city_name" :value="town.id" :key="town.id"></el-option>
                       </el-select>
                       <el-input v-model="studentInfo.address" style="width: auto;" placeholder="请输入详细地址" maxlength="100"></el-input>
                     </el-form-item>
-                    <el-form-item label="邮编：" prop="date1" label-width="150px" class="">
+                    <el-form-item label="邮编：" label-width="150px">
                             <el-input v-model="studentInfo.zip_code" placeholder="请输入邮编"></el-input>
                     </el-form-item>
                 </el-form>
@@ -212,7 +213,7 @@ export default {
               { required: true, message: '请填写姓名', trigger: 'blur' }
           ],
           enter_date: [
-              { type: 'date', required: true, message: '请选择日期', trigger: 'blur' }
+              { required: true, message: '请选择日期', trigger: 'blur' }
           ],
           certificate_type: [
               { required: true, message: '请选择证件类型', trigger: 'blur' }
@@ -221,7 +222,7 @@ export default {
               { required: true, message: '请填写证件号', trigger: 'blur' }
           ],
           date_of_birth: [
-              { type: 'date', required: true, message: '请选择出生日期', trigger: 'blur' }
+              {  required: true, message: '请选择出生日期', trigger: 'blur' }
           ],
           gender: [
               { required: true, message: '请选择性别', trigger: 'blur' }
@@ -235,22 +236,34 @@ export default {
       allOptions_url:'/api/common/select/all_list/',
       city_url:'/api/common/select/city_list/',/*省市*/
       year_url:'/api/common/select/academic_year_list/',/*学年*/
+      saveStudent:'/api/student/student/'+this.$route.params.id+'/',
+      newType:['is_employee_child'],
+      preferred_grade_type_name:'',
+      preferred_class_type_name:'',
       studentInfo:{
-        name:'',
-        nick_name:'',
-        enter_date:'',
-        first_pay_date:'',
-        certificate_type:'',
-        certificate_no:'',
-        type:'',
-        nationality:'',
-        religion:'',
-        date_of_birth:'',
-        gender:'',
-        primary_language:'',
-        other_language:''
+        name: '',
+        nick_name: '',
+        enter_date: '',
+        first_pay_date: '',
+        certificate_type: '',
+        certificate_no: '',
+        nationality: '',
+        religion: '',
+        date_of_birth: '',
+        gender: '',
+        is_employee_child: 0,
+        is_owner: 0,
+        has_siblings_in_eton: 0,
+        primary_language: '',
+        other_language: '',
+        remark: '',
+        town: '',
+        address: '',
+        zip_code: '',
+        preferred_class: 1,
       },
       city_list:[],
+      class_list:[],
       Language_options:[],/*语言*/
       Belief_options:[],/*宗教*/
       Country_options:[],/*国家*/
@@ -259,6 +272,7 @@ export default {
       provinceList:[],
       cityList:[],
       townList:[],
+      p_city_id:'',
       c_city_id:'',
       town_id:'',
     }
@@ -304,7 +318,7 @@ export default {
       this.$axios.get(this.city_url,{
         params:{
           level:2,
-          father_id:_this.studentInfo.p_city_id
+          father_id:_this.p_city_id
         }
       }).then(res=>{
         _this.cityList = res.data.results;
@@ -375,9 +389,48 @@ export default {
       _this.$axios.get(this.student_url).then(res=>{
         _this.loading = false;
         if(res.status == 200 && res.data.status_code == 1) {
-          this.studentInfo = res.data.detail;
-          this.studentInfo.nationality = parseInt(this.studentInfo.nationality);
-          console.log(this.studentInfo);
+          this.class_list = res.data.detail.class_list;
+          this.studentInfo.name = res.data.detail.name;
+          this.studentInfo.nick_name = res.data.detail.nick_name;
+          this.studentInfo.enter_date = res.data.detail.enter_date;
+          this.studentInfo.first_pay_date = res.data.detail.first_pay_date;
+          this.studentInfo.certificate_type = res.data.detail.certificate_type;
+          this.studentInfo.certificate_no = res.data.detail.certificate_no;
+          this.studentInfo.nationality = parseInt(res.data.detail.nationality);
+          this.studentInfo.religion = res.data.detail.religion;
+          this.studentInfo.date_of_birth = res.data.detail.date_of_birth;
+          this.studentInfo.gender = res.data.detail.gender;
+          this.studentInfo.is_employee_child = res.data.detail.is_employee_child;
+          this.studentInfo.is_owner = res.data.detail.is_owner;
+          this.studentInfo.has_siblings_in_eton = res.data.detail.has_siblings_in_eton;
+          this.studentInfo.primary_language = res.data.detail.primary_language;
+          this.studentInfo.other_language = res.data.detail.other_language;
+          this.studentInfo.remark = res.data.detail.remark;
+          this.studentInfo.town = res.data.detail.town;
+          this.studentInfo.address = res.data.detail.address;
+          this.studentInfo.zip_code = res.data.detail.zip_code;
+          this.studentInfo.preferred_class = res.data.detail.preferred_class;
+          this.preferred_grade_type_name = res.data.detail.preferred_grade_type_name;
+          this.preferred_class_type_name = res.data.detail.preferred_class_type_name;
+          this.newType = [];
+          var falsg = false;
+          if(res.data.detail.is_employee_child == 1){
+            this.newType.push('is_employee_child');
+            falsg = true;
+          }
+          if(res.data.detail.is_owner == 1){
+            this.newType.push('is_owner');
+            falsg = true;
+          }
+          if(res.data.detail.has_siblings_in_eton == 1){
+            this.newType.push('has_siblings_in_eton');
+            falsg = true;
+          }
+          if(falsg === false){
+            this.newType.push('is_employee_child');
+          }
+          this.p_city_id = res.data.detail.province_city.province;
+          this.c_city_id = res.data.detail.province_city.city;
         }
       }).catch(err=>{
         console.log(err)
@@ -386,9 +439,35 @@ export default {
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          alert('submit!');
+          for(var x in this.newType){
+            if(this.newType[x] === 'is_employee_child'){
+              this.studentInfo.is_employee_child = 1;
+            }
+            if(this.newType[x] === 'is_owner'){
+              this.studentInfo.is_owner = 1;
+            }
+            if(this.newType[x] === 'has_siblings_in_eton'){
+              this.studentInfo.has_siblings_in_eton = 1;
+            }
+          }
+          this.$axios.put(this.saveStudent,this.studentInfo).then(res=>{
+            if(res.status == 200){
+              this.$message({
+                type:'success',
+                message:'编辑成功！'
+              })
+              /*this.getOptions();
+              this.getCountryOptions();
+              this.getIntercity_list();
+              this.getYearList();*/
+              this.getStudentInfo();
+            }else{
+              this.$message.error('编辑失败');
+            }
+          }).catch(err=>{
+            this.$message.error('编辑失败');
+          })
         } else {
-          console.log('error submit!!');
           return false;
         }
       });
@@ -410,7 +489,7 @@ export default {
     }
   },
   watch: {
-    'studentInfo.p_city_id'(){
+    'p_city_id'(){
       this.getcity_list();
     },
     'c_city_id'(){
