02da7939eb90a6bc3eab7f42c92288de6ff83a71
Unknown <1475574795@qq.com>
2019-05-24 15:29:15 +0800

    a

--- src/components/finance/reserveFundSchool/list.vue
+++ src/components/finance/reserveFundSchool/list.vue
@@ -6,30 +6,31 @@
     <div class="content">
       <div class="select-header">
         <span style="margin-left: 10px">学年：</span>
-        <el-select v-model="nameSelect" placeholder="请选择">
+        <el-select v-model="academic_year_id" placeholder="请选择">
+          <el-option value="" label="全部"></el-option>
           <el-option
-            v-for="item in options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value">
+            v-for="item in year_list"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id">
           </el-option>
         </el-select>
         <span style="margin-left: 10px">班级：</span>
-        <el-select v-model="nameSelect" placeholder="请选择">
+        <el-select v-model="class_id" placeholder="请选择">
           <el-option
-            v-for="item in options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value">
+            v-for="item in class_list"
+            :key="item.id"
+            :label="item.center_class__name"
+            :value="item.id">
           </el-option>
         </el-select>
-        <span style="margin-left: 20px">学生姓名</span>
-        <el-input v-model="input" placeholder="输入关键字" style="width: 15%;"></el-input>
-        <span class="padding-left-30"><el-button type="primary" @click="searchList">搜索</el-button></span>
-        <span class="right" style="cursor:pointer" @click="$router.push('/financemanagement/create-reservefund')">
+        <span style="margin-left: 20px">学生姓名：</span>
+        <el-input v-model="search_name" placeholder="请输入学生姓名" class="search_input"></el-input>
+        <span class="padding-left-30"><el-button type="primary" @click="getList">搜索</el-button></span>
+        <!--<span class="right" style="cursor:pointer" @click="$router.push('/financemanagement/create-reservefund')">
             <i class="icon-font fa fa-calendar-plus-o"></i>
             <span class="font-cl-blue font-size-14">推迟入园转备用金</span>
-        </span>
+        </span>-->
         <span class="right" style="cursor:pointer" @click="$router.push('/financemanagement/create-reserve')">
             <i class="icon-font fa fa-calendar-plus-o"></i>
             <span class="font-cl-blue font-size-14">缺勤请假转备用金</span>
@@ -37,13 +38,13 @@
       </div>
       <div class="list-content">
         <el-table
-          :data="chargeTableDate"
+          :data="tableDate"
           border
           stripe
           show-header
           style="width: 100%;margin-top: 10px;">
           <el-table-column
-            prop="code"
+            prop="student_no"
             label="学号">
           </el-table-column>
           <el-table-column
@@ -51,64 +52,82 @@
             label="姓名">
           </el-table-column>
           <el-table-column
-            prop="intercity_name"
+            prop="student_class"
             label="所在班级">
           </el-table-column>
           <el-table-column
             label="初期余额">
             <el-table-column
-              prop="province"
               label="普惠">
+              <template slot-scope="scope">
+                <span>{{scope.row.puhui.init}}</span>
+              </template>
             </el-table-column>
             <el-table-column
-              prop="city"
               label="非普惠">
+              <template slot-scope="scope">
+                <span>{{scope.row.not_puhui.init}}</span>
+              </template>
             </el-table-column>
           </el-table-column>
           <el-table-column
             label="本学年结转额">
             <el-table-column
-              prop="province"
               label="普惠">
+              <template slot-scope="scope">
+                <span>{{scope.row.puhui.exchange}}</span>
+              </template>
             </el-table-column>
             <el-table-column
-              prop="city"
               label="非普惠">
+              <template slot-scope="scope">
+                <span>{{scope.row.not_puhui.exchange}}</span>
+              </template>
             </el-table-column>
           </el-table-column>
           <el-table-column
             label="本学年待批使用">
             <el-table-column
-              prop="province"
               label="普惠">
+              <template slot-scope="scope">
+                <span>{{scope.row.puhui.pending}}</span>
+              </template>
             </el-table-column>
             <el-table-column
-              prop="city"
               label="非普惠">
+              <template slot-scope="scope">
+                <span>{{scope.row.not_puhui.pending}}</span>
+              </template>
             </el-table-column>
           </el-table-column>
           <el-table-column
-            prop="telephone"
             label="本学年已使用">
             <el-table-column
-              prop="province"
               label="普惠">
+              <template slot-scope="scope">
+                <span>{{scope.row.puhui.used}}</span>
+              </template>
             </el-table-column>
             <el-table-column
-              prop="city"
               label="非普惠">
+              <template slot-scope="scope">
+                <span>{{scope.row.not_puhui.used}}</span>
+              </template>
             </el-table-column>
           </el-table-column>
           <el-table-column
-            prop="status_name"
             label="可用余额">
             <el-table-column
-              prop="province"
               label="普惠">
+              <template slot-scope="scope">
+                <span>{{scope.row.puhui.available}}</span>
+              </template>
             </el-table-column>
             <el-table-column
-              prop="city"
               label="非普惠">
+              <template slot-scope="scope">
+                <span>{{scope.row.not_puhui.available}}</span>
+              </template>
             </el-table-column>
           </el-table-column>
           <el-table-column
@@ -116,10 +135,8 @@
             label="操作"
             width="180">
             <template slot-scope="scope">
-              <el-button type="text" size="small" @click="editSchool(scope.row,0)">
-                待批记录
-                </el-button>&nbsp;&nbsp;&nbsp;|
-              <el-button type="text" size="small" @click="editSchool(scope.row,1)">使用记录</el-button>
+              <el-button type="text" size="small">待批记录</el-button>&nbsp;&nbsp;&nbsp;|
+              <el-button type="text" size="small">使用记录</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -132,62 +149,71 @@
   export default {
     data() {
       return {
-        nameSelect:[],
-        options: [{
-          value: '选项1',
-          label: '黄金糕'
-        }, {
-          value: '选项2',
-          label: '双皮奶'
-        }, {
-          value: '选项3',
-          label: '蚵仔煎'
-        }, {
-          value: '选项4',
-          label: '龙须面'
-        }, {
-          value: '选项5',
-          label: '北京烤鸭'
-        }],
-        value1:'',
-        value2:'',
-        input:'',
-        activeIndex:'1',
-        chargeTableDate:[
-          {
-            code:'xxxxxxxxxxxx',
-            name:'31231231',
-            intercity_name:'312313',
-            hq_name:'31231',
-            opening_date:'31231',
-            leader:'31231',
-            telephone:'312312',
-            status_name:'12312313',
-          },
-          {
-            code:'xxxxxxxxxxxx',
-            name:'31231231',
-            intercity_name:'312313',
-            hq_name:'31231',
-            opening_date:'31231',
-            leader:'31231',
-            telephone:'312312',
-            status_name:'12312313',
-          }
-        ]
-      };
+        year_list: [],
+        academic_year_id: '',
+        class_list: [],
+        class_id: '',
+        tableDate: [],
+        search_name: ''
+      }
+    },
+    mounted: function () {
+      this.getYearList()
+    },
+    watch: {
+      academic_year_id() {
+        this.class_id = ''
+        this.getClassList()
+      },
+      class_id() {
+        this.getList()
+      }
     },
     methods: {
-      handleClose (){
-
+      /*学年*/
+      getYearList: function () {
+        this.$axios.get('/api/common/select/academic_year_list/')
+          .then(res => {
+            this.year_list = res.data.results
+            for (var x in this.year_list) {
+              if (this.year_list[x].is_selected === 1) {
+                this.academic_year_id = this.year_list[x].id
+              }
+            }
+            this.getClassList()
+          }).catch(err => {
+          console.log(err)
+        })
       },
-      searchList: function() {
-
+      /*班级*/
+      getClassList: function () {
+        this.$axios.get('/api/attendance/students_attendance/classes/?academic_year_id=' + this.academic_year_id)
+          .then(res => {
+            this.class_list = res.data.data.classes
+            this.class_id = this.class_list[0].id
+            this.getList()
+          }).catch(err => {
+          console.log(err)
+        })
       },
-      editSchool:function (param,index) {
-        if(index === 0){
+      getList: function () {
+        this.$axios.get('/api/finance/reserved_fund/', {
+          params: {
+            academic_year_id: this.academic_year_id,
+            class_id: this.class_id,
+            search_name: this.search_name
+          }
+        })
+          .then(res => {
+            this.tableDate = res.data.results
+          }).catch(err => {
+
+        })
+      },
+      editSchool: function (param, index) {
+        if (index === 0) {
           this.$router.push('/financemanagement/pendingApprovalRecord');
-        }else{
+        } else {
           this.$router.push('/financemanagement/usageRecord');
         }
       }
@@ -196,26 +222,31 @@
 </script>
 
 <style scoped>
-  .discountlist{
+  .discountlist {
     color: rgba(160, 160, 160, 1);
     text-align: left;
   }
-  .discountlist .bold{
+
+  .discountlist .bold {
     font-weight: bold;
     color: black;
   }
-  .discountlist .select-header,.refundlist .list-content{
+
+  .discountlist .select-header, .refundlist .list-content {
     width: 100%;
     min-height: 50px;
     margin-top: 20px;
     line-height: 50px;
   }
-  .discountlist .el-select{
+
+  .discountlist .el-select {
     width: 10%;
   }
-  .discountlist >>> .el-table .cell, .el-table th div, .el-table--border td:first-child .cell, .el-table--border th:first-child .cell{
+
+  .discountlist >>> .el-table .cell, .el-table th div, .el-table--border td:first-child .cell, .el-table--border th:first-child .cell {
     text-align: center !important;
   }
+
   .discountlist >>> .el-input__inner {
     width: -webkit-fill-available;
   }
