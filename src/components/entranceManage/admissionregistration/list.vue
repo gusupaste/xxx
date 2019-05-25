3326f6136aae4938e534fe0b2622660675fb069d
Unknown <1475574795@qq.com>
2019-05-25 10:25:45 +0800

    a

--- src/components/entranceManage/admissionregistration/list.vue
+++ src/components/entranceManage/admissionregistration/list.vue
@@ -5,6 +5,7 @@
       </div>
       <div class="content">
         <div class="select-header">
+          <span>搜索：</span>
           <el-input v-model="input" placeholder="输入学号、学生姓名或者学生卡号" class="search_input"></el-input>
           <span class="padding-left-30"><el-button type="primary" @click="searchList">搜索</el-button></span>
         </div>
