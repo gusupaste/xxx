9d089c21178bb7aecd77828828bc3c9c2ebd40b2
mn <1076660242@qq.com>
2019-05-29 14:52:22 +0800

    commit

--- src/components/studentManage/studentTypeList/graduating_class.vue
+++ src/components/studentManage/studentTypeList/graduating_class.vue
@@ -34,13 +34,13 @@
             <el-input v-model="searchText" placeholder="输入学号、学生姓名或者学生卡号" class="w250_input"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="getStudentList">搜索</el-button>
+            <el-button type="primary" @click="getStudentList()">搜索</el-button>
           </el-form-item>
         </el-form>
 
       </div>
       <div class="studentFileList">
-          <div class="studentFileCard left" v-for="(item , index) in student_list " :key="index" @click="$router.push('/studentFile/studentFileDetail/'+item.id)">
+          <div class="studentFileCard" v-for="(item , index) in student_list " :key="index" @click="$router.push('/studentFile/studentFileDetail/'+item.id)">
             <div style="padding:20px">
               <div class="avatar inline-block">
                 <img src="../../../assets/img/logo.png" alt="">
@@ -67,11 +67,22 @@
             </div>
           </div>
       </div>
+    <el-pagination
+      background
+      layout="prev,pager, next, jumper"
+      :current-page="currentPage"
+      :page-size="10"
+      @current-change="changePage"
+      :total="total">
+    </el-pagination>
   </div>
 </template>
 <style scoped>
   .student_class >>> .el-form-item__label{
     padding: 0;
+  }
+  .student_class >>> .el-pagination{
+    white-space: pre-line;
   }
   .student_class .studentFileList {
     margin-top: 20px;
@@ -89,6 +100,7 @@
     -webkit-box-shadow: rgba(11,98,137,.2) 0px 0px 4px;
     -moz-box-shadow: rgba(11,98,137,.2) 0px 0px 4px;
     box-shadow: rgba(11,98,137,.2) 0px 0px 4px;
+    display: inline-block;
   }
   .student_class .studentFileCard .card-content{
     margin-left: 20px;
@@ -132,6 +144,10 @@ export default {
     activeTabs:{
       type:String,
       request:true,
+    },
+    total:{
+      type:Number,
+      request:true,
     }
   },
   data(){
@@ -139,6 +155,7 @@ export default {
       class_val:'',
       gender:'',
       dateValue: [],
+      currentPage:1,
       searchText:'',
          form: {
           name: '',
@@ -153,6 +170,9 @@ export default {
     }
   },
   methods:{
+    changePage(val){
+      this.currentPage = val;
+    },
     getStudentList:function () {
       var data={
         student_type:'Graduating',
@@ -161,6 +181,7 @@ export default {
         date_to:this.dateValue[1],
         gender:this.gender,
         condition:this.searchText,
+        page:this.currentPage
       }
       this.$emit('getStudentList',data);
     },
@@ -173,6 +194,9 @@ export default {
         }
       },
       deep: true
+    },
+    currentPage(){
+      this.getStudentList()
     }
   }
 }
